f17c2a0ae4a1b1de7753211993397598ac2a2628
NoobZombieTR <38327391+NoobZombieTR@users.noreply.github.com>
2018-04-25 21:14:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ var msg = message.content.toUpperCase();
   var sender = message.author;
 
 	
+	
 	if(message.content.includes("http")){
 	if(message.member.hasPermission("MANAGE_MESSAGES")) return;
   	message.delete()
